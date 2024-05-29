65e72dfac84f77eeb05e8fcd54b93b7c84111b87
tompilsbury <tompilsbury8@gmail.com>
2024-05-29 22:22:46 +0100

    Added an SQLite3 database to store item price history. Price history for items can be queried through /price-history/:sku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const sqlite3 = require('sqlite3').verbose();
 const socketIo = require('socket.io');
 const { getPrice } = require('./getPrice');
 const schedule = require('node-schedule');
@@ -46,6 +47,19 @@ app.route('/items/:sku')
     }
 });
 
+app.get('/price-history/:sku', (req, res) => {
+  const sku = req.params.sku;
+  const db = new sqlite3.Database('./prices.db');
+  db.all(`SELECT * FROM prices WHERE sku = ? ORDER BY timestamp`, [sku], (err, rows) => {
+    if (err) {
+      res.status(500).send('Error querying the database');
+    } else {
+      res.json(rows);
+    }
+    db.close();
+  });
+});
+
 io.on('connection', (socket) => {
     console.log('A client connected');
 
@@ -58,61 +72,52 @@ io.on('connection', (socket) => {
     })
 })
 
-
-
-function processItemInWorker(sku) {
-    return new Promise((resolve, reject) => {
-      const worker = new Worker('./src/getPriceWorker.js', { workerData: sku });
-      worker.on('message', resolve);
-      worker.on('error', reject);
-      worker.on('exit', (code) => {
-        if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
-      });
-    });
-  }
   
-//   const pLimit = require('p-limit');
+async function backgroundTask() {
+  try {
+    const data = await fs.readFile(`${config.pathToPricelist}/pricelist.json`, 'utf8');
+    const items = JSON.parse(data);
+    const queue = Object.keys(items);
 
-async function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
-  async function backgroundTask() {
-    try {
-      const data = await fs.readFile(`${config.pathToPricelist}/pricelist.json`, 'utf8');
-      const items = JSON.parse(data);
-      const queue = Object.keys(items);
-  
-      const processQueue = async () => {
-        while (queue.length > 0) {
-          const sku = queue.shift();
-          try {
-            console.log(`GETTING PRICE FOR ${sku}`);
-            const price = await getPrice(sku);
-            io.emit('price', price);
-            if (price && price.buy && price.sell) {
-              console.log(`Emitted {price: buy: {keys: ${price.buy.keys}, metal: ${price.buy.metal}}, sell: {keys: ${price.sell.keys}, metal: ${price.sell.metal}}} for item ${price.sku}`);
-            } else {
-              console.error('Error: Price object or its properties are undefined.');
-            }
-          } catch (error) {
-            console.error(error);
+    const db = new sqlite3.Database('./prices.db');
+
+    const processQueue = async () => {
+      while (queue.length > 0) {
+        const sku = queue.shift();
+        try {
+          console.log(`GETTING PRICE FOR ${sku}`);
+          const price = await getPrice(sku);
+          io.emit('price', price);
+          if (price && price.buy && price.sell) {
+            console.log(`Emitted {price: buy: {keys: ${price.buy.keys}, metal: ${price.buy.metal}}, sell: {keys: ${price.sell.keys}, metal: ${price.sell.metal}}} for item ${price.sku}`);
+
+            db.run(`INSERT INTO prices (sku, buy_keys, buy_metal, sell_keys, sell_metal) VALUES (?, ?, ?, ?, ?)`, 
+              [sku, price.buy.keys, price.buy.metal, price.sell.keys, price.sell.metal], 
+              (err) => {
+                if (err) {
+                  console.error('Error inserting data into the database', err);
+                } else {
+                  console.log(`Inserted price for ${sku} in the database`);
+                }
+              }
+            );
+          } else {
+            console.error('Error: Price object or its properties are undefined.');
           }
-          await new Promise(resolve => setTimeout(resolve, 2000));
+        } catch (error) {
+          console.error(error);
         }
-      };
-  
-      await processQueue();
-    } catch (error) {
-      console.error('Error reading pricelist file');
-      console.error(error);
-    }
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      }
+    };
+
+    await processQueue();
+  } catch (error) {
+    console.error('Error reading pricelist file');
+    console.error(error);
   }
+}
   
-  
-  
-
-
 backgroundTask();
 schedule.scheduleJob('*/30 * * * *', backgroundTask);
 
